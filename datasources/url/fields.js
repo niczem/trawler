22d7bec77c1ac89952134b7d0b28208295819ad1
niczem <ahab@systemli.org>
2021-01-05 10:53:03 +0100

    url module added

---
+++ datasources/url/fields.js
@@ -0,0 +1,49 @@
+module.exports = [
+  {
+    identifier: 'url',
+    fields: [
+      {
+        name: 'identifier',
+        title: 'Identifier(s)',
+        type: 'text',
+      },
+      {
+        name: 'type',
+        title: 'Type',
+        type: 'select',
+        options: {
+          1: 'json'
+        },
+      },
+      {
+        name: 'url',
+        title: 'URL',
+        type: 'text',
+      },
+      {
+        name: 'pagination',
+        title: 'Pagination',
+        type: 'select',
+        options: {
+          true: 'yes',
+          false: 'no'
+        },
+      },
+        {
+          name: 'pagination_min',
+          title: 'Pagination Minimum (usually 0 or 1)',
+          type: 'number',
+        },
+        {
+          name: 'pagination_max',
+          title: 'Pagination Maximum',
+          type: 'number',
+        },
+        {
+          name: 'result_index',
+          title: 'Result Index (array index or object index in which the results are stored)',
+          type: 'text',
+        }
+    ],
+  },
+];
