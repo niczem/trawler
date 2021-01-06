b0f958d76a04382462845b219c5ec87d6b8bca43
niczem <ahab@systemli.org>
2021-01-06 04:37:33 +0100

    prettier

--- src/components/datasets/Overview.vue
+++ src/components/datasets/Overview.vue
@@ -50,8 +50,8 @@
                 fill-rule="evenodd"
                 d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"
               />
-            </svg>
-          </a>&nbsp;
+            </svg> </a
+          >&nbsp;
           <a href="#" v-on:click="deleteDataset(crawl.filename)">
             <svg
               xmlns="http://www.w3.org/2000/svg"
