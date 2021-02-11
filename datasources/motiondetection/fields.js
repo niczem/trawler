b3f81c81a5aa2fdeb62a6745c0a51a698eacec33
niczem <ahab@systemli.org>
2021-02-11 12:22:06 +0100

    datasource for motiondetection added

---
+++ datasources/motiondetection/fields.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    identifier: 'motiondetection',
+    fields: [
+      {
+        name: 'identifier',
+        title: 'Identifier(s)',
+        type: 'text',
+      },
+      {
+        name: 'path',
+        title: 'Path',
+        type: 'text',
+      },
+    ],
+  },
+];
