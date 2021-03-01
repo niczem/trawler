923e09f3f49cd7aa898f712612937bf6366afbb3
niczem <ahab@systemli.org>
2021-03-01 19:45:44 +0100

    logview added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+});
