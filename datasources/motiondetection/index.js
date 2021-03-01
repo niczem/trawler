b7b8719c5f018d9174874a90e76ef4dc79d0d03a
niczem <ahab@systemli.org>
2021-03-01 19:42:25 +0100

    poc for crop

--- datasources/motiondetection/index.js
+++ datasources/motiondetection/index.js
@@ -3,7 +3,7 @@
  */
 
 const path = require('path');
-
+const getDimensions = require('get-video-dimensions');
 const Worker = require(path.resolve(__dirname, '../../utils/worker.js'));
 
 require('dotenv').config();
@@ -36,11 +36,16 @@ module.exports = class Datasource extends Worker {
 
             const arrayOfFiles = job.properties.path.split(',');
 
-            console.log(arrayOfFiles);
+            let crop = false;
+            if(job.properties.areas.length > 0){
+              crop = {
+                areas:job.properties.areas,
+                totalwidth:job.properties.totalwidth,
+                totalheight:job.properties.totalheight,
+              }
+            }
 
             for (let i in arrayOfFiles) {
-              console.log('======', arrayOfFiles[i], '======');
-
               self.addJob({
                 type: 'motiondetection_videofile',
                 identifier: arrayOfFiles[i],
@@ -49,10 +54,12 @@ module.exports = class Datasource extends Worker {
                 filename: arrayOfFiles[i],
                 results_dir: results_dir,
                 results_file: results_file,
+                crop:crop
               });
             }
             cb(null, []);
           };
+
           run(job.properties.url, function (err, result) {
             if (err) {
               console.log(err);
@@ -83,6 +90,20 @@ module.exports = class Datasource extends Worker {
             console.log('starting to analyse video');
 
             const shell = require('shelljs');
+
+            let h_ratio,v_ratio;
+
+            if(job.properties.crop){
+              const dimensions = await getDimensions(job.properties.path);
+              h_ratio = dimensions.width/job.properties.crop.totalwidth;
+              v_ratio = dimensions.height/job.properties.crop.totalheight;
+              let area = job.properties.crop.areas[0]
+              let cropped_path = job.properties.path.replace(/(\.[\w\d_-]+)$/i, '_cropped$1');;
+
+              let command = `ffmpeg -i ${job.properties.path} -filter:v "crop=${area.width*h_ratio}:${area.height*v_ratio}:${area.x*h_ratio}:${area.y*v_ratio}" ${cropped_path}`
+              await self.runShellCommand(command, job.id);
+              job.properties.path = cropped_path;
+            }
 
             console.log(job.properties.path);
             console.log(job.properties.filename);
