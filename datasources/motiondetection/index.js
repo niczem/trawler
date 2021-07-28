af3e8db786bb5d7157c1a36937e15a1d7114c1ed
niczem <ahab@systemli.org>
2021-07-28 02:00:34 +0200

    prettier:write

--- datasources/motiondetection/index.js
+++ datasources/motiondetection/index.js
@@ -37,12 +37,12 @@ module.exports = class Datasource extends Worker {
             const arrayOfFiles = job.properties.path.split(',');
 
             let crop = false;
-            if(job.properties.areas&&job.properties.areas.length > 0){
+            if (job.properties.areas && job.properties.areas.length > 0) {
               crop = {
-                areas:job.properties.areas,
-                totalwidth:job.properties.totalwidth,
-                totalheight:job.properties.totalheight,
-              }
+                areas: job.properties.areas,
+                totalwidth: job.properties.totalwidth,
+                totalheight: job.properties.totalheight,
+              };
             }
 
             for (let i in arrayOfFiles) {
@@ -55,7 +55,7 @@ module.exports = class Datasource extends Worker {
                 outputpath: job.properties.outputpath,
                 results_dir: results_dir,
                 results_file: results_file,
-                crop:crop
+                crop: crop,
               });
             }
             cb(null, []);
@@ -92,58 +92,54 @@ module.exports = class Datasource extends Worker {
 
             const shell = require('shelljs');
 
-
-
-
-            let py_cb = async function (videopath){
-              
+            let py_cb = async function (videopath) {
               const script_path = path.resolve(
                 __dirname,
                 './video-analyzer-toolkit/main.py'
               );
 
               let outputpath = '';
-              console.log(job.properties)
-              if(job.properties.outputpath){
+              console.log(job.properties);
+              if (job.properties.outputpath) {
                 outputpath = `--output_path ${job.properties.outputpath[0]}`;
               }
 
               let command = `python3 ${script_path} -v "${videopath}" -u ${job.properties.results_dir} -i -j ${job.properties.results_file} ${outputpath}`;
-              
 
               await self.runShellCommand(command, job.id, (res) => {
                 console.log('done');
                 console.log('remove cropped video');
 
-                if(job.properties.crop){
-                    fs.unlink(videopath);
+                if (job.properties.crop) {
+                  fs.unlink(videopath);
                 }
 
                 cb(null, []);
               });
+            };
 
-            }
+            let h_ratio, v_ratio;
 
-
-
-            let h_ratio,v_ratio;
-
-            if(job.properties.crop){
+            if (job.properties.crop) {
               const dimensions = await getDimensions(job.properties.path);
-              h_ratio = dimensions.width/job.properties.crop.totalwidth;
-              v_ratio = dimensions.height/job.properties.crop.totalheight;
-              let area = job.properties.crop.areas[0]
-              let cropped_path = job.properties.path.replace(/(\.[\w\d_-]+)$/i, '_cropped$1');;
+              h_ratio = dimensions.width / job.properties.crop.totalwidth;
+              v_ratio = dimensions.height / job.properties.crop.totalheight;
+              let area = job.properties.crop.areas[0];
+              let cropped_path = job.properties.path.replace(
+                /(\.[\w\d_-]+)$/i,
+                '_cropped$1'
+              );
 
-              let command = `ffmpeg -loglevel error -i ${job.properties.path} -filter:v "crop=${area.width*h_ratio}:${area.height*v_ratio}:${area.x*h_ratio}:${area.y*v_ratio}" ${cropped_path}`
-              await self.runShellCommand(command, job.id, function(){
+              let command = `ffmpeg -loglevel error -i ${
+                job.properties.path
+              } -filter:v "crop=${area.width * h_ratio}:${
+                area.height * v_ratio
+              }:${area.x * h_ratio}:${area.y * v_ratio}" ${cropped_path}`;
+              await self.runShellCommand(command, job.id, function () {
                 py_cb(cropped_path);
               });
               //job.properties.path = cropped_path;
-            }else
-              py_cb(job.properties.path)
-
-
+            } else py_cb(job.properties.path);
           };
           run(job.properties.url, function (err, result) {
             if (err) {
