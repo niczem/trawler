9368378cbd4b823e4a3f45b05c0e9aef3a9eb440
niczem <ahab@systemli.org>
2021-07-28 02:33:26 +0200

    route /jobs/:id/status added

--- api.js
+++ api.js
@@ -99,6 +99,27 @@ app.get('/jobs/:id', function (req, res) {
 
 	res.send(db.read().get('jobs').find({ id: req.params.id }).value());
 });
+app.get('/jobs/:id/status', function (req, res) {
+	
+	const low = require('lowdb')
+	const FileSync = require('lowdb/adapters/FileSync')
+	const adapter = new FileSync('data/db.json')
+	const db = low(adapter)
+
+
+	let no_of_children = 0, no_of_done_children = 0;
+	let all_jobs = db.read().get('jobs').value();
+	for(let i in all_jobs){
+		if(all_jobs[i].properties&&all_jobs[i].properties.parent == req.params.id){
+			no_of_children++;
+
+			if(all_jobs[i].status == "done"){
+				no_of_done_children++;
+			}
+		}
+	}
+	res.send({no_of_children,no_of_done_children});
+});
 
 //clear jobs
 app.get('/jobs', function (req, res) {
