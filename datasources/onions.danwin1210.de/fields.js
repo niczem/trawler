ffb6dfdee9a65ccdcb3ce474f50922428faf4db5
niczem <ahab@systemli.org>
2022-10-15 14:50:27 +0200

    feature: datasource for onions.danwin1210.de added

---
+++ datasources/onions.danwin1210.de/fields.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    identifier: 'onions.danwin1210.de',
+    fields: [
+      {
+        name: 'limit',
+        title: 'Limit',
+        type: 'number',
+        step: 1,
+      },
+    ],
+  },
+];
