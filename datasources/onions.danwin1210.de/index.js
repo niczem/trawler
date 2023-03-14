1c4904230c2190cdefb4d07cb0b9279d70d5c1ad
niczem <ahab@systemli.org>
2023-03-15 00:07:40 +0100

    feature: cleanup function added

--- datasources/onions.danwin1210.de/index.js
+++ datasources/onions.danwin1210.de/index.js
@@ -2,7 +2,6 @@
  * @file **onions.danwin1210.de** download tor-catalogue from danwin1210.de, and creates screenshots of each website in the result
  */
 const path = require('path');
-
 const Worker = require(path.resolve(__dirname, '../../utils/worker.js'));
 
 require('dotenv').config();
@@ -155,7 +154,7 @@ module.exports = class Datasource extends Worker {
     return [
       {
         identifier: 'onions.danwin1210.de',
-        method: function (job, db) {
+        method: async function (job, db, cb) {
           let crawler = new onionlistCrawler();
 
           let database_name = '';
@@ -174,73 +173,76 @@ module.exports = class Datasource extends Worker {
             }
             console.log(result);
             for (let i in result.onions) {
-              console.log(result.onions[i].address);
-              console.log(i + '/' + result.onions.length);
-
-              try {
-                await crawler.takeScreenShot(
-                  `http://${result.onions[i].address}.onion`,
-                  download_path
-                );
-              } catch (e) {
-                console.log(e);
-              }
+              console.log(parseFloat(job.properties.limit));
+              if (i < parseFloat(job.properties.limit))
+                try {
+                  console.log(result.onions[i].address);
+                  console.log(i + '/' + result.onions.length);
+                  await crawler.takeScreenShot(
+                    `http://${result.onions[i].address}.onion`,
+                    download_path
+                  );
+                } catch (e) {
+                  console.log(e);
+                }else{
+                  break;
+                }
               console.log('done');
             }
 
-            let gif_move_and_compress =
-              'convert -gravity North -extent 1600x1600 -background white -delay 150 -loop 0 ' +
-              download_path +
-              '/*.png ' +
-              download_path +
-              '/animation.gif&&mogrify -format jpg ' +
-              download_path +
-              '/*.png&&rm -f ' +
-              download_path +
-              '/*.png&&mv ' +
-              download_path +
-              '/animation.gif ' +
-              download_path +
-              '/../' +
-              job.id +
-              '.gif';
-
-            let create_mp4_file_from_screenshots = `mogrify -crop 1280  ${download_path}/*.jpeg -gravity Center && convert -append ${download_path}/*.jpeg ${download_path}/out.png && ffmpeg -f lavfi -i color=s=1920x1080 -loop 1 -t 0.08 -i ${download_path}/out.png -filter_complex "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+1/0.02/TB),fps=25[fg]; [0:v][fg]overlay=y=-'t*h*0.02':eof_action=endall[v]" -map "[v]" ${download_path}/output.mp4`;
-            console.log('create gif');
-            self.runShellCommand(
-              create_mp4_file_from_screenshots,
-              job.id,
-              async function () {
-                console.log('zip screenshot dir');
-                await self.zipDir(
-                  download_path,
-                  download_path + '.zip',
-                  job.id
-                );
-                console.log('zipping done..');
-
-                console.log('delete directory...');
-                rimraf.sync(download_path);
-
-                //convert -resize 1024x1024 -delay 1 -loop 0 *.png animation.gif
-
-                fs.writeFile(
-                  path.resolve(__dirname, '../../data/' + job.id + '.json'),
-                  JSON.stringify({ data: result }, null, 2),
-                  function (err) {
-                    if (err) return console.log(err);
-                  }
-                );
-                //console.log(objectArrayToCSV({data:posts[0]}));
-                //update job
-                db.read()
-                  .get('jobs')
-                  .find({ id: job.id })
-                  .assign({ status: 'done' })
-                  .write();
+            fs.writeFile(
+              path.resolve(__dirname, '../../data/' + job.id + '.json'),
+              JSON.stringify({ data: result }, null, 2),
+              function (err) {
+                if (err) return console.log(err);
               }
             );
+            cb();
           });
+        },
+        cleanup: async function (job, db) {
+          let download_path = path.resolve(__dirname, '../../data/' + job.id);
+          let gif_move_and_compress =
+            'convert -gravity North -extent 1600x1600 -background white -delay 150 -loop 0 ' +
+            download_path +
+            '/*.png ' +
+            download_path +
+            '/animation.gif&&mogrify -format jpg ' +
+            download_path +
+            '/*.png&&rm -f ' +
+            download_path +
+            '/*.png&&mv ' +
+            download_path +
+            '/animation.gif ' +
+            download_path +
+            '/../' +
+            job.id +
+            '.gif';
+
+          let create_mp4_file_from_screenshots = `mogrify -crop 1280  ${download_path}/*.jpeg -gravity Center && convert -append ${download_path}/*.jpeg ${download_path}/out.png && ffmpeg -f lavfi -i color=s=1920x1080 -loop 1 -t 0.08 -i ${download_path}/out.png -filter_complex "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+1/0.02/TB),fps=25[fg]; [0:v][fg]overlay=y=-'t*h*0.02':eof_action=endall[v]" -map "[v]" ${download_path}/output.mp4`;
+          console.log('create gif');
+          self.runShellCommand(
+            create_mp4_file_from_screenshots,
+            job.id,
+            async function () {
+              console.log('zip screenshot dir');
+              await self.zipDir(download_path, download_path + '.zip', job.id);
+              console.log('zipping done..');
+
+              console.log('delete directory...');
+              rimraf.sync(download_path);
+
+              //convert -resize 1024x1024 -delay 1 -loop 0 *.png animation.gif
+
+              //console.log(objectArrayToCSV({data:posts[0]}));
+              //update job
+              db.read()
+                .get('jobs')
+                .find({ id: job.id })
+                .assign({ status: 'done' })
+                .write();
+            }
+          );
         },
       },
     ];
